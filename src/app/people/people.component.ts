53a5f7e331e87a4f4c1dcf2f1646413e6dae40b3
Nikolai Lillemark Børseth <nikolailb1999@gmail.com>
2025-02-13 12:35:10 +0100

    Added ids to people, so we can have multiple friends with the same name.

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { Person } from '../app.component';
+
 @Component({
   selector: 'app-people',
   standalone: true,
@@ -10,20 +12,27 @@ import { CommonModule } from '@angular/common';
 })
 export class PeopleComponent {
   @Input() id: number = 0;
-  @Input() name: string | null = null;
-  @Input() isFavorite: (name: string) => boolean = () => false;
-  @Output() favoriteSelected = new EventEmitter<string>();
-  @Output() removeFavorite = new EventEmitter<string>();
+  @Input() name: string = '';
+  @Input() isFavorite: (person: Person) => boolean = () => false;
+  @Output() favoriteSelected = new EventEmitter<Person>();
+  @Output() removeFavorite = new EventEmitter<Person>();
 
   selectFavorite() {
-    if (this.name) {
-      this.favoriteSelected.emit(this.name);
+    if (this.name && this.id) {
+      this.favoriteSelected.emit(this.asPerson());
     }
   }
 
   rmFavorite() {
-    if (this.name) {
-      this.removeFavorite.emit(this.name);
+    if (this.name && this.id) {
+      this.removeFavorite.emit(this.asPerson());
     }
+  }
+
+  asPerson(): Person {
+    return {
+      id: this.id,
+      name: this.name,
+    };
   }
 }
